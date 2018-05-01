d0f5e546ce21ffc749122ad609f2b794d15f791b
Sidney Schultz <tewzdk@gmail.com>
2018-05-01 12:38:12 +0200

    base

---
+++ src/main/java/project/panther/PantherApplication.java
@@ -0,0 +1,12 @@
+package project.panther;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PantherApplication.class, args);
+    }
+}
